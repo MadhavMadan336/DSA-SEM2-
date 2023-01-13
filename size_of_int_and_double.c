adfda3bdf4cc0bebeea29e80c883cae9466d4518
MadhavMadan336 <122387627+MadhavMadan336@users.noreply.github.com>
2023-01-13 16:47:32 +0530

    size_of_int_and_double
    SIZE OF INTIGER AND DOUBLE

---
+++ size_of_int_and_double.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=0;
+    double d=10.21;
+
+    printf("%d\n",sizeof (a));
+    printf("%d\n",sizeof (d));
+    printf("%d\n",sizeof (a+d));
+return 0;
+}
